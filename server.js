319ccedf775f6d21529a15c715892dc6abc7399c
Norman Lei <normanlei0901@gmail.com>
2019-11-23 12:38:46 -0800

    backend

--- server.js
+++ server.js
@@ -15,37 +15,35 @@ var waitlist = [];
 // Routes
 // =============================================================
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+app.get("/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
 });
-app.get("/view", function(req, res) {
+app.get("/view", function (req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
-  });
+});
 // Displays all characters
-app.get("/api/tables", function(req, res) {
-  return res.json(reservations);
+app.get("/api/tables", function (req, res) {
+    return res.json(reservations);
 });
 // Displays a single character, or returns false
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
     return res.json(waitlist);
-})
-// Create New Characters - takes in JSON input
-app.post("/add", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newReservation = req.body;
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-  console.log(newCharacter);
-  characters.push(newCharacter);
-  res.json(newCharacter);
 });
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.post("/api/tables", function (req, res) {
+    var newReservation = req.body;
+    console.log(newReservation);
+    reservations.push(newReservation);
+    res.json(newReservation);
+});
+app.post("/api/waitlist", function (req, res) {
+    var newReservation = req.body;
+    console.log(newReservation);
+    waitlist.push(newReservation);
+    res.json(newReservation);
+});
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
